6d1fc8fa822f4f9437783da818d45517fe713401
Paola Katherine <paola@hadrons.io>
2016-02-04 11:59:02 -0200

    First commit

--- www/views.py
+++ www/views.py
@@ -5,11 +5,12 @@ from django.conf import settings
 
 from react.render import render_component
 
+
 def home(request):
-	rendered = render_component(
-		os.path.join(os.getcwd(), 'www', 'static', 'www', 'index.jsx'),
+    rendered = render_component(
+        os.path.join(os.getcwd(), 'www', 'static', 'www', 'index.jsx'),
         {},
         to_static_markup=True,
     )
 
-	return HttpResponse(rendered)
+    return HttpResponse(rendered)
